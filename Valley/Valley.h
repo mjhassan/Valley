f814762d94b6921d859da8825537535cc9902cc2
Jahid Hassan <master13sust@gmail.com>
2018-11-16 16:48:38 +0600

    Initial Commit

---
+++ Valley/Valley.h
@@ -0,0 +1,19 @@
+//
+//  Valley.h
+//  Valley
+//
+//  Created by Jahid Hassan on 11/16/18.
+//  Copyright © 2018 Jahid Hassan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Valley.
+FOUNDATION_EXPORT double ValleyVersionNumber;
+
+//! Project version string for Valley.
+FOUNDATION_EXPORT const unsigned char ValleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Valley/PublicHeader.h>
+
+
